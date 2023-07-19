8c3eaa7e3799d9ba15e8066bcc3d18a5bc61fd3e
Novan R. Kuncoro <enerka4ig@gmail.com>
2023-07-19 20:01:16 +0700

    Replace static `localhost:3000` with `API_BASE_URL` from .env

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -27,7 +27,7 @@ const authOptions: AuthOptions = {
         if (!credentials?.username || !credentials.password) {
           throw new Error('Missing username or password')
         }
-        const response = await fetch('http://localhost:3000/api/auth/login', {
+        const response = await fetch(`${process.env.API_BASE_URL}/auth/login`, {
           method: 'POST',
           body: JSON.stringify({
             username: credentials?.username,
