1e9ad4af8ef29b98ba5b762c3afd3c5d74ceed63
Novan R. Kuncoro <enerka4ig@gmail.com>
2023-07-19 18:43:41 +0700

    Fix build error because a file importing authOptions

--- src/utils/getServerSession.ts
+++
@@ -1,6 +0,0 @@
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
-import { getServerSession as nextAuthGetServerSession } from 'next-auth'
-
-export async function getServerSession() {
-  return await nextAuthGetServerSession(authOptions)
-}
